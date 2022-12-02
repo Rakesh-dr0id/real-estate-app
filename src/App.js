40bf56772b331c850d139b4e761a50bf6496aa03
Rakesh <rakeshmacho2016@gmail.com>
2022-12-02 13:14:09 +0530

    Navbar 👍

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import React from 'react';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import ForgotPass from './pages/ForgotPass';
+import Offers from './pages/Offers';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="">
-      <h2>Hello World</h2>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPass />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ForgotPass.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForgotPass = () => {
+  return <div>Forgot Password</div>;
+};
+
+export default ForgotPass;
