e9477b12dbc4a920862d76c09e3af003f792808c
Rakesh <rakeshmacho2016@gmail.com>
2022-12-07 12:56:28 +0530

    Share link functionality in listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,13 +11,15 @@ import SwiperCore, {
   Pagination,
 } from 'swiper';
 import 'swiper/css/bundle';
+import { FaShare } from 'react-icons/fa';
 
 const Listing = () => {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
-  SwiperCore.use(Autoplay, Navigation, Pagination);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -57,6 +59,24 @@ const Listing = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500 " />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold z-10 border-2 border-gray-400 rounded-md bg-white p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 };
